b6a6ed1eef569a1ebda7c5f4d6f3290f515119b2
Ajay Birare <ajaybirare516@gmail.com>
2023-09-19 19:53:52 +0530

    add scrll hover effect on a tag

--- script.js
+++ script.js
@@ -13,11 +13,68 @@ btn.addEventListener("click", function () {
   btn.classList.toggle("menuImg");
 });
 
-// .profile .about .skills .education
-// .projects .contact
+// .profile .about .skills .
+// .education .contact
 
 const profile = document.querySelector(".profile");
+const about = document.querySelector(".about");
+const skills = document.querySelector(".skills");
+const projects = document.querySelector(".projects");
+const education = document.querySelector(".education");
+const contact = document.querySelector(".contact");
 
+// profile-----
 profile.addEventListener("mouseover", function () {
-  console.log("hii");
+  document.querySelector(".navbar .right a").classList.add("over");
+});
+profile.addEventListener("mouseleave", function () {
+  document.querySelector(".navbar .right a").classList.remove("over");
+});
+
+// about------
+
+about.addEventListener("mouseover", function () {
+  document.querySelector(".navbar .right .aboutover").classList.add("over");
+});
+about.addEventListener("mouseleave", function () {
+  document.querySelector(".navbar .right .aboutover").classList.remove("over");
+});
+
+// skill----
+skills.addEventListener("mouseover", function () {
+  document.querySelector(".navbar .right .skillover").classList.add("over");
+});
+skills.addEventListener("mouseleave", function () {
+  document.querySelector(".navbar .right .skillover").classList.remove("over");
+});
+
+// projects---
+projects.addEventListener("mouseover", function () {
+  document.querySelector(".navbar .right .projectover").classList.add("over");
+});
+projects.addEventListener("mouseleave", function () {
+  document
+    .querySelector(".navbar .right .projectover")
+    .classList.remove("over");
+});
+
+// education----
+education.addEventListener("mouseover", function () {
+  document.querySelector(".navbar .right .educationover").classList.add("over");
+});
+education.addEventListener("mouseleave", function () {
+  document
+    .querySelector(".navbar .right .educationover")
+    .classList.remove("over");
+});
+
+// contact----
+
+contact.addEventListener("mouseover", function () {
+  document.querySelector(".navbar .right .contactover").classList.add("over");
+});
+contact.addEventListener("mouseleave", function () {
+  document
+    .querySelector(".navbar .right .contactover")
+    .classList.remove("over");
 });
